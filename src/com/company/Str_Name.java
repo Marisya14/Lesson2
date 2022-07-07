c7703593f7975d223fbc33d739a7447c10c5f33f
Marina <galiakberova.m@mail.ru>
2022-07-07 20:20:25 +0500

    commit3

--- src/com/company/Str_Name.java
+++ src/com/company/Str_Name.java
@@ -1,8 +1,8 @@
 package com.company;
-
+// 2. Объявить строку с вашим полным именем и вывести на экран результат работы всех стандартных методов работы со строками
 public class Str_Name {
     public static void main(String[] args) {
-        String MyName = "Марина";
+        String MyName = "Марина", surName = "Валериановна", name;
         char [] ShortName = new char [4];
 
         System.out.println(MyName.length());
@@ -10,5 +10,13 @@ public class Str_Name {
         System.out.println(MyName.charAt(4));
         MyName.getChars(0,4,ShortName,0);
         System.out.println(ShortName);
+        System.out.println(MyName.replace('р','ш'));
+        System.out.println(MyName.replaceFirst("а", "А"));
+        System.out.println(MyName.replaceAll("а", "А"));
+        System.out.println(MyName.concat(" "+surName.toUpperCase()));
+        if (MyName.startsWith("М") && MyName.endsWith("а")) {
+            name=MyName.substring(1)+" родионовна";
+            System.out.println(name.toUpperCase());
+        }
     }
 }
